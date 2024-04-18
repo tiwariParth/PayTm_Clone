781f36ff0e7c408edb0b5a15f41c45aa211bfa8f
tiwariParth <243mjparth@gmail.com>
2024-04-18 21:16:28 +0530

    feat: :sparkles: adding db
    Initialize prisma and migrated the database now the application is good to go

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -11,7 +11,7 @@ interface ButtonProps {
 export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
     <button
-      className={className}
+      className={`${className} bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
