4c48a4ee37ce131c3b298c0919961f1f8983a758
tiwariParth <243mjparth@gmail.com>
2024-04-19 19:34:37 +0530

    Update package.json and schema.prisma files

--- apps/user-app/api/user/route.ts
+++ apps/user-app/api/user/route.ts
@@ -1,16 +1,16 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@repo/db/client";
+import { PrismaClient } from "@prisma/client";
 
 const client = new PrismaClient();
 
-export const GET = async()=>{
-    await client.user.create({
-        data:{
-            email:"asdf",
-            name:"asdf",
-            password:"asdf"
-
-        }
-    })
-    return NextResponse.json({message:"Hello"})
-}
+export const GET = async () => {
+  await client.user.create({
+    data: {
+      email: "asdf",
+      name: "asdf",
+      password: "asdf",
+      age: 1,
+    },
+  });
+  return NextResponse.json({ message: "Hello" });
+};
--- packages/db/prisma/migrations/20240419134516_init/migration.sql
+++ packages/db/prisma/migrations/20240419134516_init/migration.sql
@@ -1,16 +1,11 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
-    "age" INTEGER NOT NULL,
     "email" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
+    "name" TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
